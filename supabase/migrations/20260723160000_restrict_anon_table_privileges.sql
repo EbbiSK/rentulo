b9db0fd007fcb89b62c60c99622550e74cb04d5b
EbbiSK <ebbi@ebbi.sk>
2026-07-23 11:34:44 +0200

    Restrict anonymous table privileges

---
+++ supabase/migrations/20260723160000_restrict_anon_table_privileges.sql
@@ -0,0 +1,15 @@
+﻿revoke all privileges
+on table public.notifications
+from anon;
+
+revoke insert, update, delete
+on table public.offers
+from anon;
+
+revoke all privileges
+on table public.profiles
+from anon;
+
+revoke insert, update, delete
+on table public.reviews
+from anon;
