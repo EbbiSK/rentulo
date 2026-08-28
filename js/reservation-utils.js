ec30faf051856d731f82daabdb108eca341c031c
EbbiSK <ebbi@ebbi.sk>
2026-08-28 15:14:59 +0200

    Remove unused reservation date helper

--- js/reservation-utils.js
+++ js/reservation-utils.js
@@ -1,19 +1,5 @@
 "use strict";
 
-function formatDateTime(dateString) {
-  if (!dateString) {
-    return "-";
-  }
-
-  const date = new Date(dateString);
-
-  if (Number.isNaN(date.getTime())) {
-    return String(dateString);
-  }
-
-  return date.toLocaleString("cs-CZ");
-}
-
 function getStars(rating) {
   const count = Math.max(0, Math.min(5, Number(rating) || 0));
   let stars = "";
